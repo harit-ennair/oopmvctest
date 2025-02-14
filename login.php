8f15addc8386582f73a8574dbdc33e48c327cefd
harit <harit@gmail.com>
2025-02-14 15:49:45 +0100

    mvc

--- login.php
+++ login.php
@@ -8,23 +8,6 @@ if (isset($_POST["submit"])) {
     $login = new User($_POST['email'], $_POST['password']);
     $login->login();
 }
-?>
 
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <form action="" method="post">
-        <input type="email" name="email" placeholder="email" id="">
-        <input type="password" name="password" placeholder="password" id="">
-        <button type="submit" name="submit">login</button>
-    </form>
-</body>
-
-</html>
+require_once "login-vieu.php";
